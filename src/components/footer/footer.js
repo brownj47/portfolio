b31e9b23f0ba5428c4f871fcbacd9289c977cea1
Justus B <justus.c.brown@gmail.com>
2022-08-27 20:27:09 -0700

    deployment attempt #1

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,13 +6,19 @@ function Footer(){
         <>
         <div className='spacer' style={{minHeight: '40px'}}></div>
         <footer className='fixed-bottom text-center'>
-            <p className='m-2'>Made By Justus Brown in 2022</p>
-            <p><a href='https://www.linkedin.com/in/justus-b-8a1158108/' target='_blank'rel='noopener noreferrer'>LinkedIn</a></p>
-            <a href='https://github.com/brownj47' target='_blank'rel='noopener noreferrer'>Github</a>
+
+            <p className='m-2'>Made by Justus Brown in 2022</p>
+
+            <button className='btn bg-dark'>
+            <a href='https://www.linkedin.com/in/justus-b-8a1158108/' target='_blank'rel='noopener noreferrer'>LinkedIn</a>
+            </button>
+
             <button className='btn bg-dark'>
             <a href='https://github.com/brownj47' target='_blank'rel='noopener noreferrer'>Github</a>
-                
             </button>
+
+
+
         </footer>
         </>
     )
