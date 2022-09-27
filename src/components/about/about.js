10ccd0080e5a3a16d8ea4556051274e43cb01f32
Justus B <justus.c.brown@gmail.com>
2022-09-27 11:53:23 -0700

    format changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,7 +6,7 @@ function About() {
     return (
         <main className='text-center'>
             <h3>About Me</h3>
-            <br/>
+            <br />
             <div className='row content'>
                 <div className='col-1 col-md-2'></div>
                 <div className='col-10 col-md-8'>
@@ -14,18 +14,16 @@ function About() {
                 </div>
                 <div className='col-1 col-md-2'></div>
             </div>
-            <br/>
+            <br />
             <div className='row'>
                 <div className='col-md-2'></div>
                 <div className='col-md-8'>
                     <div className='about-text text-start'>
-                        <p>Welcome to my Portfolio! My name is Justus Brown. 
+                        <p>Welcome to my Portfolio! My name is Justus Brown. I am a Full Stack Web Developer with a background in Biology and Healthcare.</p>
 
-                         I am a Full Stack Web Developer with a background in Biology and Healthcare.</p>
-                         
-                         <p>I recently graduated from the University of Washington’s Full Stack Web Development Certificate Program. I am interested in roles in the intersection of technology and biology.</p>   
-                            
-                         <p>I am a creative, thorough problem-solver with a ready-to-learn attitude, and a passion for utility, beauty, and a positive user experience.</p>
+                        <p>I recently graduated from the University of Washington’s Full Stack Web Development Certificate Program. I am interested in roles in the intersection of technology and biology.</p>
+
+                        <p>I am a creative, thorough problem-solver with a ready-to-learn attitude, and a passion for utility, beauty, and a positive user experience.</p>
                     </div>
                 </div>
                 <div className='col-md-2'></div>
